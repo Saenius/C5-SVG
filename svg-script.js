bef0e76790af35da531acde777e23aeca6e006b6
Saenius <66512810+Saenius@users.noreply.github.com>
2020-10-12 11:56:36 +0300

    Update svg-script.js

--- svg-script.js
+++ svg-script.js
@@ -1,9 +1,8 @@
 const btn = document.querySelector('.j-btn-test');
-
+const icon1 = document.querySelector('.btn_icon');
+const icon2 = document.querySelector('.btn_icon2');
 
 btn.addEventListener('click', () => {
-  const icon1 = document.querySelector('.btn_icon');
-  const icon2 = document.querySelector('.btn_icon2');
   if (icon1.style.display = 'block'){
     icon1.style.display = 'none';
     icon2.style.display = 'block';
